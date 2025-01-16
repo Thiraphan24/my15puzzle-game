1dda65a8470e1ec40c3d2f2a6e1fd18bcd2003a0
Thiraphan Panyalerdsattha <thiraphan.codercamp@gmail.com>
2025-01-16 12:17:23 +0700

    Update Vite config with output directory and base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // ตั้งค่า output directory สำหรับ production build
+  },
+  base: '/', // ปรับ base path ในกรณีที่ใช้โดเมนหรือ subdirectory อื่น ๆ
 })
